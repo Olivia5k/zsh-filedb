4def5c9d1695ea5b95a80bdf9b7ef702478fc285
Lowe Thiderman <lowe.thiderman@gmail.com>
2012-07-23 19:35:17 +0200

    Streamline the actual file execution

--- core/applications.zsh
+++ core/applications.zsh
@@ -1,66 +1,16 @@
-# Smartypants sudo wrapper!
-function edit()
+function zsys-edit()
 {
-    typeset -a args files
-    local editor=${$(whence -pc $EDITOR):-vi}
-
-    for a in $*; do
-        if [[ -f "$a" ]]; then
-            # Use absolute paths. If symbolic links, follow them as long as
-            # possible.
-            files+=($a:A)
-
-            if [[ ! -w "$a" ]] && ! [[ $editor =~ "^sudo " ]]; then
-                print -Pn "%B%F{yellow}${a:a}%f%b not writable by "
-                print -P  "%B%F{green}${USER}%f%b; going sudo"
-
-                editor="sudo $editor"
-            fi
-        else
-            args+=($a)
-        fi
-    done
-
-    # Before editing, cd to the parent directory of the first file given
-    cd -q ${files[1]:h} &> /dev/null
-    ${(z)editor} $args $files
-    cd -q - &> /dev/null
+    _zsys-execute "${${ZSYS_EDITOR:-$EDITOR}:-vi}" write $*
 }
-alias e="edit"
 
-eval "alias $EDITOR=edit"
+alias e="zsys-edit"
+eval "alias $EDITOR=zsys-edit"
 
-function page()
+function zsys-log()
 {
-    typeset -a args files
-
-    if [[ "$1" = "--tail"  ]]; then
-        # Use multitail if available; otherwise tail -f
-        local pager=${$(whence -pc multitail):-tail -f}
-        shift
-    else
-        local pager=${$(whence -pc $PAGER):-less}
-    fi
-
-    for a in $*; do
-        if [[ -f "$a" ]]; then
-            # Use absolute paths. If symbolic links, follow them as long as
-            # possible.
-            files+=($a:A)
-
-            if ! [[ -r "$a" ]]; then
-                print -Pn "%B%F{yellow}${a}%f%b not readable by "
-                print -P  "%B%F{green}${USER}%f%b; going sudo"
-
-                # Only once should be enough.
-                if ! [[ $pager =~ "^sudo " ]]; then
-                    pager="sudo $pager"
-                fi
-            fi
-        else
-            args+=($a)
-        fi
-    done
-
-    ${(z)pager} $args $files
+    _zsys-execute "${${ZSYS_PAGER:-$PAGER}:-less}" read $*
+}
+function zsys-tail()
+{
+    _zsys-execute "${${ZSYS_TAILER:-$TAILER}:-tail -f}" read $*
 }
---
+++ core/helpers.zsh
@@ -0,0 +1,40 @@
+function _zsys-execute()
+{
+    typeset -a files args
+    local cmd
+
+    cmd=$1
+    action=$2
+    shift 2
+
+    for a in $*; do
+        if [[ -f "$a" ]]; then
+            # Use absolute paths. If symbolic links, follow them as long as
+            # possible.
+            files+=($a:A)
+
+            if [[ "$action" = "write" ]] && [[ ! -w "$a" ]] || \
+                [[ "$action" = "read" ]] && [[ ! -r "$a" ]]; then
+                if ! [[ $cmd =~ "^sudo " ]] ; then
+                    if [[ "$2" = "write" ]]; then
+                        verb="writable"
+                    else
+                        verb="readable"
+                    fi
+
+                    print -Pn "%B%F{yellow}${a:a}%f%b not $verb by "
+                    print -P  "%B%F{green}${USER}%f%b; going sudo"
+
+                    cmd="sudo $cmd"
+                fi
+            fi
+        else
+            args+=($a)
+        fi
+    done
+
+    # Silently cd to the directory containing the files
+    cd -q ${files[1]:h} &> /dev/null
+    ${(z)cmd} $args $files
+    cd -q - &> /dev/null
+}
